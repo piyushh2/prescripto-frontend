101bbf11f293d77359504b0bf999a393479debd9
piyushh2 <satijapiyush11@gmail.com>
2025-08-16 10:36:42 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -18,17 +18,19 @@ const App = () => {
     <div className="min-h-screen flex flex-col">
       <ToastContainer />
       <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/doctors' element={<Doctors />} />
-        <Route path='/doctors/:speciality' element={<Doctors />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/my-profile' element={<MyProfile />} />
-        <Route path='/my-appointments' element={<MyAppointments />} />
-        <Route path='/appointment/:docId' element={<Appointment />} />
-      </Routes>
+      <div className="flex-grow mx-4 sm:mx-[10%]">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/doctors' element={<Doctors />} />
+          <Route path='/doctors/:speciality' element={<Doctors />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/my-profile' element={<MyProfile />} />
+          <Route path='/my-appointments' element={<MyAppointments />} />
+          <Route path='/appointment/:docId' element={<Appointment />} />
+        </Routes>
+      </div>
       <Footer />
     </div>
   )
