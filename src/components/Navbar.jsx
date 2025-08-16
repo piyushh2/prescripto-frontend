7377e6c54c96b1eead0ef24c8aff5b432b7a2dbd
piyushh2 <satijapiyush11@gmail.com>
2025-08-16 10:45:38 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between text-sm py-4 px-4 mb-5 border-b border-b-gray-400'>
       <img className='w-50 cursor-pointer' src={assets.logo} alt="Logo" onClick={() => navigate('/')} />
-      <ul className='hidden md:flex items-start ml-6 gap-5 font-medium'>
+      <ul className='hidden md:flex items-center gap-5 font-medium'>
         <NavLink to='/'>
           <li className='py-2 text-[16px]'>HOME</li>
           <hr className='border-none outline-none h-0.5 w-4/5 m-auto hidden bg-primary' />
